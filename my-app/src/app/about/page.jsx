66e06e1c1069ed7b918a78de529bc42316ade0a9
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2026-05-21 09:52:11 +0200

    implemented routing

--- my-app/src/app/about/page.jsx
+++ my-app/src/app/about/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
 };
 
 export default AboutPage;
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,9 +1,17 @@
+import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <button className="btn btn-soft btn-primary">Button</button>
+      <ul>
+        <li>
+          <Link href="/">Home</Link><br/>
+          <Link href="/about">About</Link><br/>
+          <Link href="/about/contact">Contact</Link>
+        </li>
+      </ul>
     </div>
   );
 };
