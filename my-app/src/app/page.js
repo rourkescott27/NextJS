752923b8f7625b5a57fab3542de9b88e234423d1
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2026-05-21 09:32:22 +0200

    initial commit

---
+++ my-app/src/app/page.js
@@ -0,0 +1,11 @@
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button className="btn btn-soft btn-primary">Button</button>
+    </div>
+  );
+};
+
+export default HomePage;
