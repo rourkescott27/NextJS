f9653e4aaad21e55fd28df3238b36d6def9bfe45
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2026-05-24 20:20:14 +0200

    repo access initial

---
+++ my-app/src/app/githubusers/[user]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserReposPage = ({params:{user}}) => {
+    return (
+        <div>
+            UserReposPage
+        </div>
+    );
+};
+
+export default UserReposPage;
