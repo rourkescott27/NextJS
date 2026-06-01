a5914bd279ef11aa976997c2dc5fd39bd4cd4801
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2026-06-01 17:55:23 +0200

    improved "form"

--- my-app/src/app/components/AddBook.jsx
+++ my-app/src/app/components/AddBook.jsx
@@ -4,6 +4,32 @@ import { useState } from "react";
 
 const AddBook = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [newBook, setNewBook] = useState({
+    title: "",
+    link: "",
+    img: "",
+  });
+
+  const handleSubmitNewBook = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("/api/books", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newBook),
+    });
+
+    if (res.ok) {
+      setNewBook({
+        title: "",
+        link: "",
+        img: "",
+      });
+      setModalOpen(false);
+    }
+  };
 
   return (
     <div>
@@ -16,7 +42,7 @@ const AddBook = () => {
         className={`modal ${modalOpen ? "modal-open" : ""}`}
       >
         <div className="modal-box">
-          <form method="dialog">
+          <form method="dialog" onSubmit={handleSubmitNewBook}>
             {/* if there is a button in form, it will close the modal */}
             <button
               className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
@@ -24,8 +50,40 @@ const AddBook = () => {
             >
               ✕
             </button>
+            <h3 className="font-bold text-lg text-center">Add New Book</h3>
+            <div className="flex flex-col items-center gap-3 mt-5">
+              <input
+                type="text"
+                value={newBook.title}
+                onChange={(e) =>
+                  setNewBook({ ...newBook, title: e.target.value })
+                }
+                placeholder="Create Book Title"
+                className="input input-primary w-full max-w-xs"
+              />
+              <input
+                type="url"
+                value={newBook.img}
+                onChange={(e) =>
+                  setNewBook({ ...newBook, img: e.target.value })
+                }
+                placeholder="Image URL"
+                className="input input-primary w-full max-w-xs"
+              />
+              <input
+                type="url"
+                value={newBook.link}
+                onChange={(e) =>
+                  setNewBook({ ...newBook, link: e.target.value })
+                }
+                placeholder="Book Link"
+                className="input input-primary w-full max-w-xs"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary w-full mt-5">
+              Add Book
+            </button>
           </form>
-          <h3 className="font-bold text-lg">Add New Book</h3>
         </div>
       </dialog>
     </div>
