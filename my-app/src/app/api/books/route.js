0ec6b38b45789ff0d8257775919c5705e2302f0e
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2026-06-01 09:54:34 +0200

    book adding

--- my-app/src/app/api/books/route.js
+++ my-app/src/app/api/books/route.js
@@ -4,3 +4,16 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   return NextResponse.json(books);
 }
+
+export async function POST(request) {
+  const { title, img, link } = await request.json();
+  const newBook = {
+    id: books.length + 1,
+    title,
+    link,
+    img,
+  };
+
+  books.push(newBook);
+  return NextResponse.json("Book added successfully!");
+}
