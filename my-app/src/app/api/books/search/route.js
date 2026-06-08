4d45e267b69540c7719d6285fa0aea826ac55389
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2026-06-08 18:11:07 +0200

    refactored search functionality

--- my-app/src/app/api/books/search/route.js
+++ my-app/src/app/api/books/search/route.js
@@ -1,14 +1,19 @@
 import { NextResponse } from "next/server";
-import books from "../data.json";
+import { prisma } from "../../../db";
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get("query");
 
-  const filteredBooks = books.filter((book) => {
-    return book.title.toLowerCase().includes(query.toLowerCase());
+ const filteredBooks = await prisma.book.findMany({
+    where: {
+      title: {
+        contains: query,
+        mode: 'insensitive'
+      }
+    }
   });
-    
+
   console.log(searchParams.get("query"));
   return NextResponse.json(filteredBooks);
 }
