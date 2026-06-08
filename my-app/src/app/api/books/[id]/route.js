fbd3691694cf988960f2a271bdb570d18f1e7e63
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2026-06-08 18:10:12 +0200

    refactored delete functionality
    Now deleting from Prisma DB

--- my-app/src/app/api/books/[id]/route.js
+++ my-app/src/app/api/books/[id]/route.js
@@ -2,13 +2,13 @@ import books from "../data.json";
 import { NextResponse } from "next/server";
 
 export const DELETE = async (request, { params }) => {
-  const id = params.id;
-    //   console.log("Deleting book with id:", id);
-    const index = books.findIndex((book) => book.id === id);
-    if (index !== -1) { 
-        books.splice(index, 1);
-    }
+  const { id } = await params;
+
+  await prisma.book.delete({
+    where: { id: id },
+  });
+  
   return NextResponse.json({
-    message: `Book with id ${id} deleted successfully`
+    message: `Book with id ${id} deleted successfully`,
   });
 };
